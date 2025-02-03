6efd65fb500e2880ef628c4e4eb96eb6236eda4d
Maeevick <maeevick@gmail.com>
2025-02-03 12:58:16 +0100

    refactor: add information logs

--- src/github.ts
+++ src/github.ts
@@ -21,6 +21,7 @@ export const retrieveThreadContents = async (now: Date): Promise<Thread[]> => {
 	return await Promise.all(
 		contents
 			.filter((file) => {
+				console.log('\n---------PROCESSING CONTENT FILE---------', file.name);
 				const [, , hour, minutes] = file.name.match(/^(\d{8})T(\d{2})(\d{2})\.json$/) ?? [];
 				const [scheduledHour, scheduledMinutes] = [Number(hour), Number(minutes)];
 
@@ -28,6 +29,20 @@ export const retrieveThreadContents = async (now: Date): Promise<Thread[]> => {
 					Number(now.getUTCHours().toString().padStart(2, '0')),
 					Number(now.getUTCMinutes().toString().padStart(2, '0'))
 				];
+
+				console.log(
+					'\n---------CHECKING CONDITIONS---------\n',
+					'\n\tCHECKING TYPE:',
+					file.type === 'file',
+					'\n\tCHECKING DATE',
+					file.name.startsWith(
+						`${now.getFullYear()}${(now.getMonth() + 1).toString().padStart(2, '0')}${now.getDate().toString().padStart(2, '0')}`
+					),
+					'\n\tCHECKING EXTENSION',
+					file.name.endsWith('.json'),
+					'\n\tCHECKING TIME',
+					Math.abs(nowHour * 60 + nowMinutes - (scheduledHour * 60 + scheduledMinutes)) < 15
+				);
 				return (
 					file.type === 'file' &&
 					file.name.startsWith(
